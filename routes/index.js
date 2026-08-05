d4a5c0032fdfbda9ce7c2e477f5f1f64665ee0a5
Felipe Cuevas <ffelipecuevasc@gmail.com>
2026-08-05 09:27:54 -0300

    Inicio - Carga inicial del proyecto.

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// Importamos express y el enrutador
+import express from "express";
+const router = express.Router();
+
+// Configuración del enrutador (router) con el metodo HTPP Get (aunque existen Post, Put, Delete)
+// RUTA DE INICIO (/)
+router.get('/', (req, res) => {
+
+});
+
+export default router;
